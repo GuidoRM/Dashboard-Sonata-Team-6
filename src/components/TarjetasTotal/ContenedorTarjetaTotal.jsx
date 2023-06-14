c183438701f44c456848d5d86df6ab1ee15b0b73
Franco-Mateo-Gomez <matufranone@gmail.com>
2023-06-14 12:27:13 -0300

    Fetch de Albumes

--- src/components/TarjetasTotal/ContenedorTarjetaTotal.jsx
+++ src/components/TarjetasTotal/ContenedorTarjetaTotal.jsx
@@ -1,18 +1,33 @@
 //Componentes
 import TarjetaTotal from "./TarjetaTotal";
 import "./styles_totales.css"
+import { useState, useEffect } from 'react';
 
-function ContenedorTarjetaTotal() {
-    return (
-        <>
-            <section className="totales_contenedor">
-                <h1>Totales de Datos</h1>
-                <TarjetaTotal nombre={"Usuarios"} icono={"fa-user"} total={30}/>
-                <TarjetaTotal nombre={"Productos"} icono={"fa-compact-disc"} total={100}/>
-                <TarjetaTotal nombre={"Productos"} icono={"fa-ranking-star"} total={4}/>
-            </section>
-        </>
-    );
-}
+function ContenedorTarjetaTotal () {
+
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3030/api/products')
+        .then((response) => {
+            return response.json()
+        })
+        .then((albums) => {
+            setAlbums(albums)
+        })
+    }, [])
+
+        return (
+            <>
+                <section className="totales_contenedor">
+                    <h1>Datos del sistema</h1>
+                    <TarjetaTotal nombre={"Usuarios"} icono={"fa-user"} total={30}/>
+                    <TarjetaTotal nombre={"Productos"} icono={"fa-compact-disc"} total={albums.count}/>
+                    <TarjetaTotal nombre={"Productos"} icono={"fa-ranking-star"} total={4}/>
+                </section>
+            </>
+        );
+    }
+    
 
 export default ContenedorTarjetaTotal;
